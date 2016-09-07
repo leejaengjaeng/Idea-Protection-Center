d53aaa9803c2b9c08989051b654a674fecedf2cc
jinuk <wooyoomisutgaru@gmail.com>
2016-09-07 11:59:02 +0900

    aa

--- src/main/java/com/ipc/controller/RegistrationController.java
+++ src/main/java/com/ipc/controller/RegistrationController.java
@@ -1,5 +1,13 @@
 package com.ipc.controller;
 
-public class RegistrationController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+	@RequestMapping("/addidea")
+	public String addidea(){
+		return "registration/idea_registration";
+	}
 }
