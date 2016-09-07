eb7e66747b7020f7da02885b4508558b4531996b
jinuk <wooyoomisutgaru@gmail.com>
2016-09-07 11:50:56 +0900

    aa

---
+++ src/main/java/com/ipc/vo/RegistrationPatentVo.java
@@ -0,0 +1,179 @@
+package com.ipc.vo;
+
+public class RegistrationPatentVo {
+	int rid;
+	String typeOfInvent;
+	String title;
+	String summary;
+	String whyInvent;
+	String problem;
+	String solution;
+	String effect;
+	String core_element;
+	int uid;
+	int iscomplete;
+	int start_rid;
+	String re_typeOfInvent;
+	String re_title;
+	String re_summary;
+	String re_whyInvent;
+	String re_problem;
+	String re_solution;
+	String re_effect;
+	String re_core_element;
+	int lid;
+	String condition;
+	String pre_apply_date;
+	String apply_date;
+	String registration_date;
+	public int getRid() {
+		return rid;
+	}
+	public void setRid(int rid) {
+		this.rid = rid;
+	}
+	public String getTypeOfInvent() {
+		return typeOfInvent;
+	}
+	public void setTypeOfInvent(String typeOfInvent) {
+		this.typeOfInvent = typeOfInvent;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getWhyInvent() {
+		return whyInvent;
+	}
+	public void setWhyInvent(String whyInvent) {
+		this.whyInvent = whyInvent;
+	}
+	public String getProblem() {
+		return problem;
+	}
+	public void setProblem(String problem) {
+		this.problem = problem;
+	}
+	public String getSolution() {
+		return solution;
+	}
+	public void setSolution(String solution) {
+		this.solution = solution;
+	}
+	public String getEffect() {
+		return effect;
+	}
+	public void setEffect(String effect) {
+		this.effect = effect;
+	}
+	public String getCore_element() {
+		return core_element;
+	}
+	public void setCore_element(String core_element) {
+		this.core_element = core_element;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public int getIscomplete() {
+		return iscomplete;
+	}
+	public void setIscomplete(int iscomplete) {
+		this.iscomplete = iscomplete;
+	}
+	public int getStart_rid() {
+		return start_rid;
+	}
+	public void setStart_rid(int start_rid) {
+		this.start_rid = start_rid;
+	}
+	public String getRe_typeOfInvent() {
+		return re_typeOfInvent;
+	}
+	public void setRe_typeOfInvent(String re_typeOfInvent) {
+		this.re_typeOfInvent = re_typeOfInvent;
+	}
+	public String getRe_title() {
+		return re_title;
+	}
+	public void setRe_title(String re_title) {
+		this.re_title = re_title;
+	}
+	public String getRe_summary() {
+		return re_summary;
+	}
+	public void setRe_summary(String re_summary) {
+		this.re_summary = re_summary;
+	}
+	public String getRe_whyInvent() {
+		return re_whyInvent;
+	}
+	public void setRe_whyInvent(String re_whyInvent) {
+		this.re_whyInvent = re_whyInvent;
+	}
+	public String getRe_problem() {
+		return re_problem;
+	}
+	public void setRe_problem(String re_problem) {
+		this.re_problem = re_problem;
+	}
+	public String getRe_solution() {
+		return re_solution;
+	}
+	public void setRe_solution(String re_solution) {
+		this.re_solution = re_solution;
+	}
+	public String getRe_effect() {
+		return re_effect;
+	}
+	public void setRe_effect(String re_effect) {
+		this.re_effect = re_effect;
+	}
+	public String getRe_core_element() {
+		return re_core_element;
+	}
+	public void setRe_core_element(String re_core_element) {
+		this.re_core_element = re_core_element;
+	}
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+	public String getPre_apply_date() {
+		return pre_apply_date;
+	}
+	public void setPre_apply_date(String pre_apply_date) {
+		this.pre_apply_date = pre_apply_date;
+	}
+	public String getApply_date() {
+		return apply_date;
+	}
+	public void setApply_date(String apply_date) {
+		this.apply_date = apply_date;
+	}
+	public String getRegistration_date() {
+		return registration_date;
+	}
+	public void setRegistration_date(String registration_date) {
+		this.registration_date = registration_date;
+	}
+}
