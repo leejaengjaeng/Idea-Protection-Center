95e4092d7948f3485d75589065656dec81eae99a
JiNuk <wooyoomisutgaru@gmail.com>
2016-11-29 16:49:32 +0900

    aa

--- src/main/java/com/ipc/controller/CopyrightController.java
+++ src/main/java/com/ipc/controller/CopyrightController.java
@@ -12,11 +12,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.ipc.dao.CopyrightDao;
 import com.ipc.dao.CopyrightInfoDao;
 import com.ipc.dao.TypeOfCopyrightDao;
 import com.ipc.dao.UserDao;
+import com.ipc.service.RegistrationService;
+import com.ipc.util.CreateFileUtils;
 import com.ipc.vo.CopyRightInfoVo;
 import com.ipc.vo.CopyRightVo;
 import com.ipc.vo.userVo;
@@ -48,6 +52,14 @@ public class CopyrightController {
 	@RequestMapping(value="/regCopyright", method=RequestMethod.POST)
 	public String copyRightReg(HttpServletRequest req, Model model)
 	{
+		RegistrationService rs = new RegistrationService();
+		CreateFileUtils cfu = new CreateFileUtils();
+		
+		MultipartHttpServletRequest multipartRequest =  (MultipartHttpServletRequest)req;  //�떎以묓뙆�씪 �뾽濡쒕뱶
+		List<MultipartFile> files = multipartRequest.getFiles("plane_img");
+		String file_name="mark"+rs.getToday(1);
+		cfu.CreateFile(files.get(0), req, "/resources/uploadimgs/mark/", file_name);
+		
 		String title	= req.getParameter("idea_kind");
 		String field	= req.getParameter("field_selected");
 		String type 	= req.getParameter("kind");
--- src/main/webapp/WEB-INF/views/user/userMain.jsp
+++ src/main/webapp/WEB-INF/views/user/userMain.jsp
@@ -630,7 +630,7 @@ function downPaper(died){
 		                        <th>비고</th>
 		                    </tr>
 		                   	<c:forEach var="copyright" items="${copyrightList}" varStatus="status">
-								<tr onclick="location.href='/copyright/detail/${copyright.getCid()}'">
+								<tr onclick="location.href='/copyRight/detail/${copyright.getCid()}'">
 									<input type="hidden" value="${copyright.getCid()}"/>								
 			                        <td><p>${copyright.getTitle()}</p></td>
 			                        <td><p>${copyright.getReg_condition()}</p></td>
